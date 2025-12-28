477399c64ce6920ba59e411a2a33c0c767f5716d
Martyna <martynamazur000@gmail.com>
2025-12-28 18:32:17 +0100

    :)

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
  * Docelowo: Stripe Prices / Subscriptions
  */
 const PLANS = {
-    basic_monthly: { amount: 19.00, currency: 'pln' },
-    pro_monthly: { amount: 3999.00, currency: 'pln' },
+    basic_monthly: { amount: 1900, currency: 'pln' },
+    pro_monthly: { amount: 399900, currency: 'pln' },
 };
 
 app.post('/create-payment-intent', async (req, res) => {
